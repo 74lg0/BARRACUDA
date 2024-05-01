d5572fd9d8c856c59a894cad7ddd13ae85ef5c4b
74lg0 <111157836+74lg0@users.noreply.github.com>
2024-05-01 16:42:09 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ pip3 install threading pystyle pysocks
 pip install requests[socks]
 apt install tor
 clear
-echo "Inicia la instancia de tor en otra pestaña de terminal con el comando: \ntor"
+echo "Inicia la instancia de tor en otra pestaña de terminal con el comando: tor"

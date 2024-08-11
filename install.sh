d1b6ff77fcc5198deda60646e0d7fa87da3b436c
74lg0 <111157836+74lg0@users.noreply.github.com>
2024-08-11 14:23:40 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,55 @@
-pip3 install threading pystyle pysocks
-pip install requests[socks]
-apt install tor
-clear
-echo -e "Inicia la instancia de tor en otra pestaña de terminal con el comando:\ntor"
+#!/bin/bash
+
+# Required packages
+required_packages=(
+    "python3"
+    "tor"
+)
+
+required_pip_packages=(
+    "threading"
+    "pystyle"
+    "pysocks"
+)
+
+check_installation() {
+    which $1 > /dev/null 2>&1
+}
+
+install_package() {
+    echo -e "\e[32m[!] Instalando $1\e[0m"
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install $1
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        sudo apt-get install $1
+    fi
+}
+
+install_pip_package() {
+    echo -e "\e[32m[!] Instalando $1\e[0m"
+    pip install $1
+}
+
+main() {
+    all_installed=true
+    for package in "${required_packages[@]}"; do
+        if ! check_installation $package; then
+            all_installed=false
+            install_package $package
+        fi
+    done
+
+    for package in "${required_pip_packages[@]}"; do
+        if ! python3 -c "import $package" > /dev/null 2>&1; then
+            all_installed=false
+            install_pip_package $package
+        fi
+    done
+
+    if $all_installed; then
+        echo "Todos los requerimientos ya están instalados"
+        echo -e "Inicia una instancia de tor en otra pestaña de terminal con el comando: tor"
+    fi
+}
+
+main
